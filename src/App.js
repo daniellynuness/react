6d8eddffed96582984689c42a7d5e96a47d542c9
dani <danym.nunes@gmail.com>
2025-04-02 18:52:06 -0500

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { useState } from 'react';
 
 function App() {
+  const [contador, setContador] = useState(0);
+  function increment() {
+    setContador(contador+1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
+      <div className="App">
+      <header className="App-Header">
+      <Header title = "Guilherme lindo"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="button" value="Clique" onClick={increment} />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> {contador}</p> cliques!
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          
+          </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
