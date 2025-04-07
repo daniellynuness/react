b4637eaade3682fc346bd6c560e0c8a764daba97
dani <danym.nunes@gmail.com>
2025-04-07 18:12:14 -0500

    atualizações

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import { useState } from 'react';
+import Home from './Pages/Home'
 
 function App() {
-  const [contador, setContador] = useState(0);
-  function increment() {
-    setContador(contador+1);
-  }
   return (
-      <div className="App">
-      <header className="App-Header">
-      <Header title = "Guilherme lindo"/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <input type="button" value="Clique" onClick={increment} />
-        </p>
-        <p> {contador}</p> cliques!
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          
-          </a>
-      </header>
-    </div>
-  );
+    <Home />
+  )
 }
 
 export default App;
-
-
